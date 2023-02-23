860a4113b4d2a9b26a2e8dde6d86ec9bb30cfb63
Fabian Lopes <60930772+fabianlopes@users.noreply.github.com>
2023-02-23 00:24:00 -0400

    funcao assistente finalizada

--- db.js
+++ db.js
@@ -24,5 +24,20 @@ async function findOne(id) {
     const db = await connect();
     return db.collection(COLLECTION).findOne({ _id: new ObjectId(id) });
 }
+
+async function findExpressao(filtro) {    
+
+    const db = await connect();
+
+    const expressaoregular = [];
+    for (const chave in filtro) {
+        expressaoregular[chave] = new RegExp(filtro[chave], "i");
+      }
+    
+      console.log(expressaoregular);
+
+    return db.collection("tecnicas_tag").find( { tags: { $all: expressaoregular } } ).toArray();    
+        
+}
  
-module.exports = { findAll, findOne } 
+module.exports = { findAll, findOne, findExpressao } 
