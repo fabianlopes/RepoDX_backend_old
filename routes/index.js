bf0db27a178797c3cf916029d971c47454d20461
Fabian Lopes <60930772+fabianlopes@users.noreply.github.com>
2023-02-23 00:27:09 -0400

    funcao ok
    funcao ok

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { query } = require('express');
 var express = require('express');
 var router = express.Router();
 const db = require("../db");
@@ -26,13 +27,31 @@ router.get('/tecnicas/:id', async (req, res, next) => {
   const id = req.params.id; 
   try {
     const tecnica = await db.findOne(id);
-
-    console.log('tecnica' + ' ' +tecnica);
-
     res.render('tecnicas', { title: 'Técnicas de Avaliacao de DX', tecnica });
   } catch (err) {
     next(err);
   }
+})
+
+/* POST assistente. */ 
+router.post('/assistente', async (req, res, next) => {
+
+  try {
+    if(Object.keys(req.body).length === 0) {
+      const listatecnicas = await db.findAll();
+      res.render('assistente', { title: 'Lista de Tecnicas', listatecnicas });
+    }
+  else {
+
+      const query = Object.values(req.body);
+      const listatecnicas = await db.findExpressao(query);
+      console.log('return ' + listatecnicas + ' query ' + query);
+      res.render('assistente', { title: 'Lista de Tecnicas', listatecnicas });
+
+    }
+  } catch (err) {
+    next(err);
+  }  
 })
 
 module.exports = router;
